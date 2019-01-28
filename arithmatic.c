d23312d02daa4e8b01c4d3de784de204f32d4f7d
vishnumoorthyd <vishnumoorthy.d@vvdntech.in>
2019-01-28 18:21:31 +0530

    first commit

---
+++ arithmatic.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    clrscr();
+    printf("Enter Two Values :");
+    scanf("%d%d",&a,&b);
+    sum(a,b);
+    mult(a,b);
+    div(a,b);
+    getch();
+}
+    sum(int x,int y)
+      {
+            int z;
+            z=x+y;
+            printf("Addtion : %d\n",z);
+            return 0;
+      }
+    mult(int x,int y)
+     {
+            int z;
+            z=x*y;
+            printf("Multiply : %d\n",z);
+            return 0;
+      }
+    div(int x,int y)
+     {
+            int z;
+            z=x/y;
+            printf("Div : %d\n",z);
+            return 0;
+      }
+
